e3d8f44269319c45286837dbcfe038011c8b02ee
Anoopkumar Shukla <anoopkumarshukla111@gmail.com>
2022-01-09 22:21:04 +0530

    Updates

---
+++ precache-manifest.b64c5e28787bd5e1991e20ce2099e409.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7fd2ca54f27e1056e3d52b50b51cad5",
+    "url": "/LIVE_WEATHER/index.html"
+  },
+  {
+    "revision": "0c7dc32638f09be28eeb",
+    "url": "/LIVE_WEATHER/static/css/main.f0df3ff1.chunk.css"
+  },
+  {
+    "revision": "44d91217a2106cc453b8",
+    "url": "/LIVE_WEATHER/static/js/2.6fc8ad99.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/LIVE_WEATHER/static/js/2.6fc8ad99.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0c7dc32638f09be28eeb",
+    "url": "/LIVE_WEATHER/static/js/main.8475e74f.chunk.js"
+  },
+  {
+    "revision": "81788de09bf5342c28d0",
+    "url": "/LIVE_WEATHER/static/js/runtime-main.aad3b282.js"
+  },
+  {
+    "revision": "c97cb84b74a52edcb496295687e87822",
+    "url": "/LIVE_WEATHER/static/media/cold-bg.c97cb84b.jpg"
+  },
+  {
+    "revision": "635ed87d2cc826fb213057ac40dfaff1",
+    "url": "/LIVE_WEATHER/static/media/warm-bg.635ed87d.jpg"
+  }
+]);
